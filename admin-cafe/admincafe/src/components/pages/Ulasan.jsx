f66bdfa03dbe34b7562fb064d253e53cb167bc9b
Nipongg <abdielbungin04@gmail.com>
2025-10-30 23:58:19 +0800

    page admin-cafe

---
+++ admin-cafe/admincafe/src/components/pages/Ulasan.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Ulasan() {
+  return (
+    <div>
+      <h2>Ulasan</h2>
+      <p>Ini halaman ulasan Admin Café ☕</p>
+    </div>
+  );
+}
+
+export default Ulasan;
